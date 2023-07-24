aa8adda1c67d0479940b1cc4d0028fd7bf602c72
Yisak Girmay <yisakbruck23@gmail.com>
2023-07-24 00:31:04 -0400

    lab complete

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,21 +1,4 @@
 
-// import React from "react";
-// import QuestionItem from "./QuestionItem";
-
-// function QuestionList({ questions }) {
-//   const questionMap = questions.map(question => (
-//     <QuestionItem key={question.id} question={question} />
-//   ));
-
-//   return (
-//     <section>
-//       <h1>Quiz Questions</h1>
-//       <ul>{questionMap}</ul>
-//     </section>
-//   );
-// }
-
-// export default QuestionList;
 
 import React from "react";
 import QuestionItem from "./QuestionItem";
